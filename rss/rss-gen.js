23e5b4db45c96edda17a785bc9aa0d0e37985c32
333void <waituhokay@gmail.com>
2024-10-18 13:57:54 -0400

    idk

--- rss/rss-gen.js
+++ rss/rss-gen.js
@@ -1,9 +1,28 @@
 var client = new XMLHttpRequest();
 client.open('GET', 'index.xml');
 client.onreadystatechange = function() {
-  console.log(client.responseText);
+  // readyState 4 is for complete response
+  if (client.readyState === 4) {
+    console.log(client.responseText);
+    parseRSS(client.responseText);
+  }
 }
 client.send();
 
+function parseRSS(text) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, "application/xml");
+  // print the name of the root element or error message
+  const errorNode = doc.querySelector("parsererror");
+  if (errorNode) {
+    console.log("error while parsing");
+  } else {
+    console.log(doc);
+  }
+}
 
-const parser = new DOMParser();
+const file = new File(["heyyyyy"], "foo.txt", {
+  type: "foo.png",
+});
+
+console.log(file)
