6b25fa2086527a8c831b0d7995d62049ab7df899
333void <waituhokay@gmail.com>
2026-04-14 19:54:16 -0400

    r30 improvements WIP

--- radical30/src/r30.data.js
+++ radical30/src/r30.data.js
@@ -1,5 +1,9 @@
-export const chars = {
+export const rads = {
   // m = manner, p = place
+
+  // radicals are coded [x,y] 0 to 1
+
+  // a place/manner radical bit (ex. the p1 of ap1) off a main bit (ex. the a of ap1) is coded with the first [x,y] starting on the main bit
 
   /*
   //  COMPOUNDS
@@ -11,78 +15,125 @@ export const chars = {
 
   l: {
     shape: 'line',
-    big: [[0, 0.5], [1, 0.5]],
+    aspect: 'horizontal',
+    xy: [
+      [0, 0.5], 
+      [1, 0.5]
+    ]
   },
-  lm1: [
-    'line',
-    [0, 0.5],
-    [0, 0.5 + 1/3]
-  ],
-  lm2: [
-    'line',
-    [0.5, 0.5],
-    [0.5, 0.5 + 1/3]
-  ],
-  lm3: [
-    'line',
-    [1, 0.5],
-    [1, 0.5 + 1/3]
-  ],
-  lp1: [
-    'line',
-    [0, 0.5],
-    [0, 0.5 - 1/3]
-  ],
-  lp2: [
-    'line',
-    [0.5, 0.5],
-    [0.5, 0.5 - 1/3]
-  ],
-  lp3: [
-    'line',
-    [1, 0.5],
-    [1, 0.5 - 1/3]
-  ],
+  lm1: {
+    shape: 'line',
+    xy: [
+      [0, 0.5], 
+      [0, 1]
+    ]
+  },
+  lm2: {
+    shape: 'line',
+    xy: [
+      [0.5, 0.5], 
+      [0.5, 1]
+    ],
+    xy2: [
+      [0.5, 0.5], 
+      [0.5, 0.75]
+    ]
+  },
+  lm3: {
+    shape: 'line',
+    xy: [
+      [1, 0.5], 
+      [1, 1]
+    ]
+  },
+  lp1: {
+    shape: 'line',
+    xy: [
+      [0, 0.5], 
+      [0, 0]
+    ]
+  },
+  lp2: {
+    shape: 'line',
+    xy: [
+      [0.5, 0.5], 
+      [0.5, 0]
+    ],
+    xy2: [
+      [0.5, 0.5], 
+      [0.5, 0.25]      
+    ]
+  },
+  lp3: {
+    shape: 'line',
+    xy: [
+      [1, 0.5], 
+      [1, 0]
+    ]
+  },
 
   /*
   //  ALVEOLAR
   */
 
-  a: [
-    'line',
-    [0, 1],
-    [1, 0]
-  ],
-  ap1: [
-    'line',
-    [0, 1],
-    [0, 2/3]
-  ],
-  ap2: [
-    'line',
-    [0.5, 0.5],
-    [0.293, 0.293]
-  ],
-  ap3: [
-    'line',
-    [1, 0],
-    [2/3, 0]
-  ],
-  am1: [
-    'line',
-    [0, 1],
-    [1/3, 1]
-  ],
-  am2: [
-    'line',
-    [0.5, 0.5],
-    [0.707, 0.707]
-  ],
-  am3: [
-    'line',
-    [1, 0],
-    [1, 1/3]
-  ],
+  a: {
+    shape: 'line',
+    aspect: 'vertical',
+    xy: [
+      [0, 1], 
+      [1, 0]
+    ]
+  },
+  ap1: {
+    shape: 'line',
+    xy: [
+      [0, 1], 
+      [0, 0.5]
+    ]
+  },
+  ap2: {
+    shape: 'line',
+    xy: [
+      [0.5, 0.5], 
+      [0, 0]
+    ],
+    xy2: [
+      [0.5, 0.5], 
+      [0.25, 0.25]
+    ]
+  },
+  ap3: {
+    shape: 'line',
+    xy: [
+      [1, 0], 
+      [0.5, 0]
+    ]
+  },
+  am1: {
+    shape: 'line',
+    xy: [
+      [0, 1], 
+      [0.5, 1]
+    ]
+  },
+  am2: {
+    shape: 'line',
+    xy: [
+      [0.5, 0.5], 
+      [1, 1]
+    ],
+    xy2: [
+      [0.5, 0.5], 
+      [0.75, 0.75]
+    ]
+  },
+  am3: {
+    shape: 'line',
+    xy: [
+      [1, 0], 
+      [1, 0.5]
+    ]
+  },
   // palatal
   p: [
     'line',
@@ -101,7 +152,19 @@ export const chars = {
   */
   o: {
     shape: 'circle',
-    big: [0.5, 0.5, 0.45],
-    small: [0.5, 0.5, 1]
+    data: [0.5, 0.5, 0.45],
+  },
+  n: {
+    shape: 'polyline',
+    points: [
+      [0, 0.5],
+      [0, 0],
+      [1, 0],
+      [1, 0.5],
+      [0, 0.5],
+      [0, 1],
+      [1, 1],
+      [1, 0.5]
+    ],
   },
 }
--- radical30/src/r30.js
+++ radical30/src/r30.js
@@ -1,11 +1,11 @@
 import { bElement, bSVGElement } from "../../utils/bNodes.js";
-import { chars } from "./r30.data.js"
+import { rads } from "./r30.data.js"
 
 /*
 //  DATA 2 SVG ELEMENT
 */
 
-// measurement
+// measurement object class
 class m {
   constructor(a /* smaller */, b /* larger */) {
     this.a = a;
@@ -18,104 +18,114 @@ class m {
   }
 }
 
-function line([baseWidth, baseHeight, width, height], c, size) {
-  const w = new m (baseWidth, width);
-  const h = new m (baseHeight, height);
-  let dx = 0; 
-  let dy = 0;
-
+function line(box, rad, xy = 'xy') {
   /*
-  if (d) {
-    // reciprocal of box diagonal * size of line - size of line [to make ROC inversely proportional]
-    let roc = w.ab * (h.ab / w.ab * 0.25 - 0.25);
-    // change sign for inverted slope 
-    if (1 < Math.abs(c[2][1] - c[1][1] / c[2][0] - c[1][0])) {
-      roc = roc * -1;
-    }
+    let dx = 0;
+    let dy = 0;
+    if (d) {
+      // reciprocal of box diagonal * size of line - size of line [to make ROC inversely proportional]
+      let roc = w.ab * (h.ab / w.ab * 0.25 - 0.25);
+      // change sign for inverted slope 
+      if (1 < Math.abs(c[2][1] - c[1][1] / c[2][0] - c[1][0])) {
+        roc = roc * -1;
+      }
 
-    dx = roc;
-    dy = -roc;
+      dx = roc;
+      dy = -roc;
 
-    if (h.x(c[2][1]) + dy > height || h.x(c[2][1]) + dy < baseHeight) {
-      //let lessenFactor = (h.x(c[2][1]) + dy) - height;
-      let lessenFactor = Math.abs(((h.ab - w.ab) + 230) / 2);
-
-
-      console.log(dy, h.x(c[2][1]), (h.x(c[2][1]) + dy), height)
-
-      console.log(lessenFactor, ((h.ab - w.ab) + 230) / 2)
+      if (h.x(c[2][1]) + dy > height || h.x(c[2][1]) + dy < baseHeight) {
+        //let lessenFactor = (h.x(c[2][1]) + dy) - height;
+        let lessenFactor = Math.abs(((h.ab - w.ab) + 230) / 2);
 
 
-      dx -= lessenFactor * (h.ab / w.ab);
-      dy -= lessenFactor;
-    }
+        console.log(dy, h.x(c[2][1]), (h.x(c[2][1]) + dy), height)
+
+        console.log(lessenFactor, ((h.ab - w.ab) + 230) / 2)
 
 
+        dx -= lessenFactor * (h.ab / w.ab);
+        dy -= lessenFactor;
+      }
 
-    
+      function lFactor(mM, d, m) {
+        if (mM + d > m) {
+          let lessenFactor = (w.x(c[2][0]) + dx) - width;
+          console.log('w', lessenFactor)
+          dx -= lessenFactor;
+          dy -= lessenFactor * (w.ab / h.ab);
+        }
+      }
 
-    /*
-    function lFactor(mM, d, m) {
-      if (mM + d > m) {
+      // shorten up line if out of bounds of the box
+      if (w.x(c[2][0]) + dx > width) {
         let lessenFactor = (w.x(c[2][0]) + dx) - width;
         console.log('w', lessenFactor)
         dx -= lessenFactor;
         dy -= lessenFactor * (w.ab / h.ab);
       }
+      if (h.x(c[2][1]) + dy > height || h.x(c[2][1]) + dy < baseHeight) {
+        let lessenFactor = (h.x(c[2][1]) + dy) - height;
+        console.log('w', lessenFactor)
+        dx -= lessenFactor * (h.ab / w.ab);
+        dy -= lessenFactor;
+      } 
     }
 
-    // shorten up line if out of bounds of the box
-    if (w.x(c[2][0]) + dx > width) {
-      let lessenFactor = (w.x(c[2][0]) + dx) - width;
-      console.log('w', lessenFactor)
-      dx -= lessenFactor;
-      dy -= lessenFactor * (w.ab / h.ab);
+    if (false) {
+      // if height of box is shorter than length of box
+      if (h.ab < w.ab) {
+        dx = 
+          // total x length of box * total x length of line
+          (w.ab * (c[2][0] - c[1][0])) 
+          // x^2 - 1, x from 1 to 0 using the slope
+          * (((h.ab / w.ab) ** 2) - 1);
+      } else {
+        dy = (h.ab * (c[2][1] - c[1][1])) * (((w.ab / h.ab) ** 2) - 1);        
+      }
     }
-    if (h.x(c[2][1]) + dy > height || h.x(c[2][1]) + dy < baseHeight) {
-      let lessenFactor = (h.x(c[2][1]) + dy) - height;
-      console.log('w', lessenFactor)
-      dx -= lessenFactor * (h.ab / w.ab);
-      dy -= lessenFactor;
-    } 
-  }
-  */
+  */ 
 
-  if (false) {
-    // if height of box is shorter than length of box
-    if (h.ab < w.ab) {
-      dx = 
-        // total x length of box * total x length of line
-        (w.ab * (c[2][0] - c[1][0])) 
-        // x^2 - 1, x from 1 to 0 using the slope
-        * (((h.ab / w.ab) ** 2) - 1);
-    } else {
-      dy = (h.ab * (c[2][1] - c[1][1])) * (((w.ab / h.ab) ** 2) - 1);        
-    }
-  }
+  //let { x1, y1, x2, y2 } = box.points(rad.xy)
+
+  /*console.log(box.m + (
+    box.dx * rad.xy[0][0]),
+    box.m + (box.dy * rad.xy[0][1]),
+    box.m + (box.dx * rad.xy[1][0]),
+    box.m + (box.dy * rad.xy[1][1]),
+  )*/
 
   return new bSVGElement({
     tag: 'line',
     attributes: {
       class: "r30",
-      x1: w.x(c[size][0][0]),
-      y1: h.x(c[size][0][1]),
-      x2: w.x(c[size][1][0]) + dx,
-      y2: h.x(c[size][1][1]) + dy,
+      x1: box.ox + (box.dx * rad[xy][0][0]),
+      y1: box.oy + (box.dy * rad[xy][0][1]),
+      x2: box.ox + (box.dx * rad[xy][1][0]),
+      y2: box.oy + (box.dy * rad[xy][1][1]),
     },
   }).set();
 }
 
-function polyline([baseWidth, baseHeight, width, height], ...Cs) {
-  const w = new m (baseWidth, width);
-  const h = new m (baseHeight, height);
-
+function polyline(box, ...polyOrder) {
   let points = [];
 
-  for (const c of Cs) {
-    for (let i = 1; i < c.length; i++) {
+  if (polyOrder > 1) {
+    for (const radbit of polyOrder) {
+      //console.log('c', radbit[0])
+      // add x,y points in order specified by polyPocket function
+      for (let i = 1; i < radbit.length; i++) {
+        //console.log('i', radbit[0].xy[radbit[i]])
+        points.push(
+          box.ox + (box.dx * radbit[0].xy[radbit[i]][0]),
+          box.oy + (box.dy * radbit[0].xy[radbit[i]][1])
+        )
+      }
+    }
+  } else {
+    for (let lo of polyOrder[0].points) {
       points.push(
-        w.x(c[0][c[i]][0]),
-        h.x(c[0][c[i]][1]),
+        box.ox + (box.dx * lo[0]),
+        box.oy + (box.dy * lo[1])
       )
     }
   }
@@ -149,141 +159,138 @@ function circle([baseWidth, baseHeight, width, height], c, size) {
 //  HTML RENDERING
 */
 
+// split text string into array
 function decode(text) {
   let rawRads = text.split(/-/);
   let rads = [];
 
   // iterate over each encoded radical  
   for (const rad of rawRads) {
+    let r = rad;
     // if possibly a compound rad (starts with a compound initial letter)
-    if (/^[lapt]/.test(rad)) {
-      // test if is a compound rad (contains follow up letter and digit)
-      if ((/[mp][\d]/).test(rad)) {
-        // make array of each piece from the compound rad
-        let arr = rad.match(/([mp][\d])/g)
-        arr.unshift(rad[0])
-        rads.push(arr);
-        continue
-      }
+    // test if is a compound rad (contains follow up letter and digit)
+    if ((/^[lapt][mp][\d]/).test(rad)) {
+      // make array of each piece from the compound rad
+      r = [rad[0]].concat(rad.match(/([mp][\d])/g));
     }
 
-    rads.push(rad);
+    rads.push(r);
   }
 
-  //console.log(rads)
-  return rads
+  return (rads)
 }
 
-function charSwitch(rad, box, size) {
-  switch (chars[rad].shape) {
+// create svg shape from shape specified in r30.data
+// shape is drawn into character using data stored in box object
+function shapeSwitch(rad, box) {
+  switch (rads[rad].shape) {
     case 'line':
       return line(
         box,
-        chars[rad],
-        size
+        rads[rad]
       )
+    case 'polyline':
+      return polyline(
+        box,
+        rads[rad]
+      )
+      /*
     case 'circle':
       return circle(
         box,
-        chars[rad],
-        size
+        rads[rad]
       )
+        */
   }
 }
 
 // render multiple line pieces as a polyline correctly
 function polyPocket(rad, box) {
   let shapes = [];
-  let one = [];
-  let two = [];
-  let three = [];
+  // start with main radbit (a, l, etc.)
+  let polyOrder = [[rads[rad[0]], 1, 0]];
 
+  console.log(rad.filter((r) => r[1] == 2).length == (rad.length - 1))
+
+  let soFar = '';
   for (const piece of rad) {
+    soFar += piece;
+    console.log('cool', soFar.match(/3/g) ? [0, 1, 0] : [1, 0])
     switch (piece[1]) {
+      case '1':
+        // push polyline points in correct order, 1 manner/place is pushed to end of polyline
+        polyOrder.push(
+          [rads[rad[0] + piece], 0, 1] // 0,1 order is not relevant here like it is in manner/place 3
+        );
+        break;
       case '2':
-        two.push(piece);
+        // render m/p 2 as single line, not needed to be in polyline
         shapes.push(line(
           box,
-          chars[rad[0] + piece],
-          // if diagonal has to be re-sloped
-          rad[0] == 'a' || rad[0] == 'p' ? true : false
+          rads[rad[0] + piece],
+          // shorten m/p 2 radbits if both are present and also the only two on main radbit
+          rad.filter((r) => r[1] == 2).length == (rad.length - 1) && rad.length == 3 ? 'xy2' : 'xy'
         ));
         break;
-      case '1':
-        one.push(piece)
-        break;
       case '3':
-        three.push(piece)
+        // push polyline points in correct order, 3 manner/place is unshifted to start at beginning of polyline
+        polyOrder.unshift(
+          [rads[rad[0] + piece], 0, 1]
+          // if there is already a 3 manner/place rendered, render current m/p (the one being unshifted to come before) 
+          // to retrun to starting postion to start polyline marker to draw next m/p in sequence
+          // .concat(soFar.match(/3/g) ? [0, 1] : [1, 0])
+        );
         break;
     }
   }
 
-  let Cs = [[chars[rad[0]], 2, 1]];
-
-  // push polyline points in correct order
-  switch (three.length) {
-    case (2):
-      Cs.unshift([chars[rad[0] + three[1]], 1, 2, 1]);
-    case (1):
-      Cs.unshift([chars[rad[0] + three[0]], 2, 1]);
-  }
-
-  switch (one.length) {
-    case (2): {
-      Cs.push([chars[rad[0] + one[1]], 1, 2, 1]);
-    }
-    case (1):
-      Cs.push([chars[rad[0] + one[0]], 1, 2]);
-  }
-
-  if (Cs.length > 1) {
-    shapes.push(polyline(box, ...Cs));
-  } else {
-    // draw the compound base piece (l/a/p/t) (m/p piece pushed in switch)
-    shapes.push(charSwitch(rad[0], box));
-  }
+  shapes.push(polyline(box, ...polyOrder));
 
   return shapes;
 }
 
 class Box {
   constructor(dmsn) {
-    this.w = dmsn.w;
-    this.l = dmsn.l;
-    this.x1 = dmsn.margin;
-    this.y1 = dmsn.margin;
-    this.x2 = dmsn.w - dmsn.margin;
-    this.y2 = dmsn.l - dmsn.margin;
-    this.x = this.x2 - this.x1;
-    this.y = this.y2 - this.y1;
+    this.w = dmsn.width;
+    this.l = dmsn.length;
+    this.m = dmsn.margin;
+    // offset
+    this.ox = this.m;
+    this.oy = this.m;
+    this.dx = this.w - (this.m * 2);
+    this.dy = this.l - (this.m * 2);
   }
 
-  resize(bX1, bY1, bX2, bY2) {
-    return [
-      this.x1 + (this.x * bX1) + (this.x1 / 2 * Math.ceil(bX1)), 
-      this.y1 + (this.y * bY1) + (this.y1 / 2 * Math.ceil(bY1)), 
-      this.x2 + (this.x * (bX2 - 1)) - (this.x1 / 2 * Math.ceil(Math.abs(bX2 - 1))), 
-      this.y2 + (this.y * (bY2 - 1)) - (this.y1 / 2 * Math.ceil(Math.abs(bY2 - 1))),
-    ];
-  }
+  resize(x1, y1, x2, y2) {
+    // offset box by x1 & y1 ammount
+    this.ox = this.m + (this.dx * x1);
+    this.oy = this.m + (this.dy * y1);
 
-  get box() {
-    return [this.x1, this.y1, this.x2, this.y2];
+    console.log((this.dx))
+
+    // shorten dimensions of box by offset & by x2,y2
+    this.dx -= (this.dx / (1 / x1)) + (this.dx * (1 - x2));
+    this.dy -= (this.dy / (1 / y1)) + (this.dy * (1 - y2));
+
+    /*
+      this.m + (this.dx * x1) + (this.m / 2 * Math.ceil(x1)), 
+      this.m + (this.dy * y1) + (this.m / 2 * Math.ceil(y1)), 
+      (this.width - this.m) + (this.dx * (x2 - 1)) - (this.m / 2 * Math.ceil(Math.abs(x2 - 1))), 
+      (this.length - this.m) + (this.dy * (y2 - 1)) - (this.m / 2 * Math.ceil(Math.abs(y2 - 1))),
+    */
   }
 }
 
+// create character
 function char(text, dmsn) {
-  let rads = decode(text);
   let shapes = [];
-  let b = new Box(dmsn);
-  let size = 'big';
+  let rads = decode(text);
   
-  let i = 0;
+  //let i = 0;
   for (const rad of rads) {
-    let box = b.box;
-    if (rads.length > 1) {
+      let box = new Box(dmsn);
+    /*if (rads.length > 1) {
       i += 1;
-      size = 'small'
       switch (rads.length) {
         case 2:
           switch (i) {
@@ -294,22 +301,27 @@ function char(text, dmsn) {
 
         }
       }
-    }
+    }*/
+    // if multiple radicals
 
+    console.log('rad:', rad)
+
+    //box.resize(0, 0.5, 1, 1)
+    
     if (Array.isArray(rad)) {
-      shapes.push(...polyPocket(rad, box))
+      shapes.push(...polyPocket(rad, box)) // ap1, lp3m1, etc.
     } else {
-      shapes.push(charSwitch(rad, box, size));
-    }    
+      shapes.push(shapeSwitch(rad, box)); // l, a, o, etc.
+    }
   }
 
   return shapes;
 }
 
-// length/width, margin
+// dimensions for svg
 let dmsn = {
-  l: 500,
-  w: 500,
+  length: 500,
+  width: 500,
   margin: 40,
 };
 
@@ -318,11 +330,13 @@ let svg = new bSVGElement({
   attributes: {
     version: "1.1",
     xmlns: "http://www.w3.org/2000/svg",
-    width: dmsn.w,
-    height: dmsn.l,
+    width: dmsn.width,
+    height: dmsn.length,
   },
 }).set();
 
-svg.append(...char("l", dmsn));
+svg.append(...char("n", dmsn));
+
+// "s1-s2-s3-s4"
  
 document.getElementById("con-r30").appendChild(svg);
